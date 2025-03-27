8b705186f4a48bd6cda6e992f947497baada0af6
Pratham Surati <prathamsuraati27@gmail.com>
2025-03-27 21:20:41 +0530

    registation page

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const express = require('express');
-const userModel = require('./models/user')
-
 const app = express();
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.get('/', (req,res)=>{
-    res.send("hello");
+    res.render("index");
+});
+
+app.post('/register', async (req,res)=>{
+    let {username, email, password, name, age} = req.body;
+    let user = await userModel.findOne({email});
+    if(user) return res.status(400).send("User already exists"); 
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, async function(err, hash) {
+           let user = await userModel.create({
+                username,
+                name,
+                age,
+                email,
+                password : hash
+            });
+            // console.log(user.password);
+        });
+        let token = jwt.sign({email : email, userid : user._id}, "pratham");
+        res.cookie("token", token);
+        res.send("User registered successfully");
+    });
+
+
 });
 
 
--- models/user.js
+++ models/user.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:3000/27017/mini-project');
+mongoose.connect('mongodb://localhost:27017/mini-project');
 
 const userSchema = mongoose.Schema({
+    username : String,
     name : String,
+    age: Number,
     email : String,
+    password : String,
+    post : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'post'
+        }
+    ]
 
 });
 
