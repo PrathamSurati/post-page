b8a42b7f7928b511e1706e1b95225bce1bfef66e
Pratham Surati <prathamsuraati27@gmail.com>
2025-03-28 12:57:19 +0530

    post creation done

--- app.js
+++ app.js
@@ -1,45 +1,92 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userModel = require('./models/user');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.get('/', (req,res)=>{
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post('/register', async (req,res)=>{
-    let {username, email, password, name, age} = req.body;
-    let user = await userModel.findOne({email});
-    if(user) return res.status(400).send("User already exists"); 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(password, salt, async function(err, hash) {
-           let user = await userModel.create({
-                username,
-                name,
-                age,
-                email,
-                password : hash
-            });
-            // console.log(user.password);
-        });
-        let token = jwt.sign({email : email, userid : user._id}, "pratham");
-        res.cookie("token", token);
-        res.send("User registered successfully");
+app.get("/profile", isLoggedin, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate("posts");   // this line  not only send te id but  that send the content of the post
+  res.render("profile", { user });
+});
+
+app.post("/post", isLoggedin, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  })
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+
+app.post("/register", async (req, res) => {
+  let { username, email, password, name, age } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(400).send("User already exists");
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      let user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userid: user._id }, "pratham");
+      res.cookie("token", token);
+      res.send("User registered successfully");
+      // console.log(user.password);
     });
+  });
+});
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(400).send("Something went wrong");
 
-
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userid: user._id }, "pratham");
+      res.cookie("token", token);
+      res.status(200).redirect("/profile");
+    } else res.redirect("/login");
+  });
 });
 
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+function isLoggedin(req, res, next) {
+  if (req.cookies.token === "") res.redirect("/login");
+  else {
+    let data = jwt.verify(req.cookies.token, "pratham");
+    req.user = data;
+    next();
+  }
+}
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
